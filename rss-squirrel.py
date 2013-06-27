c7d999ab678d031844eef2756fd3a2ef27b087d5
michal-at-git <phantom-black@myopera.com>
2013-06-27 18:33:47 +0200

    nowy układ + przycisku i ikonki

--- rss-squirrel.py
+++ rss-squirrel.py
@@ -36,6 +36,11 @@ class rss_squirrel(QDialog):
         self.przejdz = QPushButton(u'Otwórz');
         self.zpliku = QPushButton(u'z pliku');
         self.zamknij = QPushButton(u'Zamknij');
+        self.dodaj = QPushButton();
+        self.dodaj.setIcon(QIcon("plus.png"))
+        self.usun = QPushButton();
+        self.usun.setIcon(QIcon("minus.png"))
+
         self.lista = QListWidget();
         
         
@@ -43,31 +48,36 @@ class rss_squirrel(QDialog):
         
         
         for i in feedr().flist:
-	  
+
 	  self.lista.addItem(i);
-        
-        
-        
-        
-        
-        
-        
-        #void itemClicked (QListWidgetItem *) // WAZNY SYGNAL
-        
+
+	  
         
         layout = QVBoxLayout()
         linlayout = QHBoxLayout();
         linlayout2 = QHBoxLayout();
 	linlayoutcont = QHBoxLayout();
-        
+        listlayout = QVBoxLayout();
+	addrem = QHBoxLayout();
+
+        	
         layout.addLayout(linlayout);
 	layout.addLayout(linlayoutcont)
+	
+	linlayoutcont.addLayout(listlayout)
 	
         linlayout.addWidget(self.zpliku)
         linlayout.addWidget(self.zamknij)
         self.lista.setMaximumWidth(200)
-   
-        linlayoutcont.addWidget(self.lista);
+        listlayout.addWidget(self.lista)
+	
+        listlayout.addLayout(addrem)
+        
+        addrem.addWidget(self.dodaj);
+
+        addrem.addWidget(self.usun);
+
+                
         linlayoutcont.addWidget(self.widok);
 
         layout.addLayout(linlayout2);
@@ -80,23 +90,23 @@ class rss_squirrel(QDialog):
 	self.setGeometry(400, 180, 600,500)
         self.setWindowTitle("RSS Squirrel")
         self.setWindowIcon(QIcon('ikonka.png'))
-	
+
 	self.connect(self.przejdz, SIGNAL("clicked()"), self.run)
 	self.connect(self.zpliku, SIGNAL("clicked()"), self.filerss)
 	self.connect(self.zamknij, SIGNAL("clicked()"), rsssq.quit)
 	self.lista.itemActivated.connect(self.openfeed)
-	
+
     def openfeed(self, lista):
       self.run("feeds/"+lista.text()+".rss")
-	
+
     def run(self, adr=None):
      
       try:
-	
+
 	if (adr == None):
 	  x = nut(self.adres.text());
 	else: x=nut(adr)
-	
+
 	self.widok.setText("<h1>"+x.frss.feed.title+"</h1>");
 	i = 0;
 	self.setWindowTitle(x.frss.feed.title+" | RSS Squirrel")
@@ -104,10 +114,10 @@ class rss_squirrel(QDialog):
 	  self.widok.append("<h2>"+x.frss.entries[i].title+"</h2>");
 	  self.widok.append(x.frss.entries[i].description);
 	  i += 1
-	  
+
       except:
 	self.widok.setText("<h1>"+u'Nie można załadować kanału rss'+"</h1>")
-	
+
     def filerss(self):
        
        plik = QFileDialog.getOpenFileName(self, '', '.')
